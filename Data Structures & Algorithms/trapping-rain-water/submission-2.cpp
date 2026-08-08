2db467465bf27019e99d8affdeae7ba9939155ff
Anupama Suresbabu Kodoth <59738300+ankh77sb@users.noreply.github.com>
2026-08-08 18:37:59 +0530

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+
+        int n = height.size();
+        if(n < 3) return 0;
+        vector<int> nextLarger(n, 0);
+        vector<int> prevLarger(n, 0);
+        
+        int currmax = height[n-1];
+     
+        for(int i = n - 2 ; i>= 0; i--) {
+            nextLarger[i] = currmax;
+            currmax = max(currmax, height[i]);
+        }
+        
+        currmax = height[0];
+
+        for(int i = 1; i < n; i++) {
+            prevLarger[i] = currmax;
+            currmax = max(currmax, height[i]);
+        }
+
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            res += max(0, min(prevLarger[i], nextLarger[i]) - height[i]);
+        }
+        
+        return res;
+    }
+};
